a9daf599fc0b0ac4357d5f1bee3b3fc5cfecc750
rezaikram <muhammadikramriza@gmail.com>
2023-07-02 15:19:31 +0700

    void preorder

--- binarysearchtree_107/binarysearchtree_107.cpp
+++ binarysearchtree_107/binarysearchtree_107.cpp
@@ -86,4 +86,19 @@
 			}
 		}
 
+		void  preorder(Node* ptr)
+		{
+			if (ROOT == NULL)
+			{
+				cout << "Tree is empty" << endl;
+				return;
+			}
+			if (ptr != NULL)
+			{
+				cout << ptr->info << " ";
+				preorder(ptr->leftchild);
+				preorder(ptr->rightchild);
+			}
+		}
+
 		
