e3bb405c9e306cd096ab82a48d12599d9c6b76de
rezaikram <muhammadikramriza@gmail.com>
2023-07-02 15:19:55 +0700

    void postorder

--- binarysearchtree_107/binarysearchtree_107.cpp
+++ binarysearchtree_107/binarysearchtree_107.cpp
@@ -101,4 +101,21 @@
 			}
 		}
 
-		
+		void postorder(Node* ptr)
+		{
+			//performs the postorder traversal of the tree
+			if (ROOT == NULL)
+			{
+				cout << "Tree is empty" << endl;
+				return;
+			}
+			if (ptr != NULL)
+			{
+				postorder(ptr->leftchild);
+				postorder(ptr->rightchild);
+				cout << ptr->info << " ";
+			}
+		}
+	};
+
+	
